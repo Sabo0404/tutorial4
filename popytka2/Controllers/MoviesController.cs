accadd01dc8aa8c4ed392bb4cafec8412269e98e
Sabina <sabina.abdimutal@gmail.com>
2020-05-09 20:12:07 +0600

    First commit

---
+++ popytka2/Controllers/MoviesController.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using popytka2.Data;
+using popytka2.Models.Movies;
+
+namespace popytka2.Controllers
+{
+    public class MoviesController : Controller
+    {
+        private readonly MoviesContext _dbContext;
+
+        public MoviesController(MoviesContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var movies = await _dbContext.Movies.ToListAsync();
+            return View(movies);
+        }
+
+        [HttpGet]
+        public IActionResult Add()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Add(Movie movie)
+        {
+
+
+            _dbContext.Movies.Add(movie);
+            await _dbContext.SaveChangesAsync();
+
+            var movies = await _dbContext.Movies.ToListAsync();
+
+            return View("Index", movies);
+        }
+
+        public async Task<IActionResult> Search(string text)
+        {
+            text = text.ToLower();
+            var searchedMovies = await _dbContext.Movies.Where(movie => movie.Name.ToLower().Contains(text)
+                                            || movie.Genre.ToLower().Contains(text)
+                                            || movie.Author.ToLower().Contains(text))
+                                        .ToListAsync();
+            return View("Index", searchedMovies);
+        }
+
+    }
+
+}
---
+++ popytka2/Data/MoviesContext.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.EntityFrameworkCore;
+using popytka2.Models.Movies;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace popytka2.Data
+{
+    public class MoviesContext : DbContext
+    {
+        public MoviesContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Movie> Movies { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Movie>().HasData(new Movie
+            {
+                Id = -1,
+                Author = "Todd Phillips",
+                Genre = "Crime , Drama , Thriller",
+                Name = "Joker",
+                Poster = "https://upload.wikimedia.org/wikipedia/en/e/e1/Joker_%282019_film%29_poster.jpg",
+                CreatedAt = new DateTime(2019, 10, 3)
+            },
+            new Movie
+            {
+                Id = -2,
+                Author = "David Leitch",
+                Genre = "Action , Adventure",
+                Name = "Fast & Furious Presents: Hobbs & Shaw",
+                Poster = "https://upload.wikimedia.org/wikipedia/en/0/00/Fast_%26_Furious_Presents_Hobbs_%26_Shaw_-_theatrical_poster.jpg",
+                CreatedAt = new DateTime(2019, 8, 2)
+            },
+            new Movie
+            {
+                Id = -3,
+                Author = "Jon Favreau",
+                Genre = "Adventure , Animation , Drama , Family , Musical",
+                Name = "The Lion King",
+                Poster = "https://www.joblo.com/movie-posters/2019/the-lion-king/image-35365#image-35365",
+                CreatedAt = new DateTime(2019, 7, 19)
+            },
+            new Movie
+            {
+                Id = -4,
+                Author = "Joachim Rønning",
+                Genre = "Adventure , Family , Fantasy",
+                Name = "Maleficent: Mistress of Evil",
+                Poster = "https://upload.wikimedia.org/wikipedia/ru/b/b0/Maleficent_-_Mistress_of_Evil_%28poster%29.jpg",
+                CreatedAt = new DateTime(2019, 10, 18)
+            });
+        }
+    }
+
+
+}
